84f015fa57cab4c606d111140a1139740bdefff6
Rahmad <azanirahmad51@gmail.com>
2021-09-26 10:29:06 +0700

    test1

---
+++ components/radiobutton1.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="form-check form-check-inline">
+      <input
+        class="form-check-input"
+        type="checkbox"
+        id="inlineCheckbox1"
+        value="option1"
+      />
+      <label class="form-check-label" for="inlineCheckbox1">1</label>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <radiobutton1 />
+    <buttonkiri />
+    <buttonkanan />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
